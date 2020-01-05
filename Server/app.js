6c52c1f25582ba144b1afd2a8bacb2535e30f0cf
AminesCodes <aminebensalem@pursuit.org>
2020-01-05 10:41:12 -0500

    added the basics of multer to app (backend)

--- Server/app.js
+++ Server/app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const corse = require('cors');
+const multer = require('multer');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,10 +19,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('/public', path.join(__dirname, 'public'))); // IGNORE /public
+app.use(cors());
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./public/images")
+//   },
+//   filename: (req, file, cb) => {
+//     let name = Date.now() + "-" + file.originalname
+//     cb(null, name)
+//   }
+// });
+// const upload = multer({ storage: storage });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// app.post('/upload', upload.single("image"), (req, res, next) => {
+//   console.log('req.file', req.file)
+//   console.log('req.body', req.body)
+//   let imageUrl = "http://localhost:3129/" + req.file.path.replace('public/', '')
+//   res.json({
+//     imageUrl: imageUrl,
+//     message: "file uploaded"
+//   })
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
